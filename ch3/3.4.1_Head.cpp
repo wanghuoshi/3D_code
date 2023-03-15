3c42e6021c51201f268089d07399ff882d918682
WangLei <wanglei@qq.com>
2023-03-15 17:35:08 +0800

    20230315

--- ch3/3.4.1_Head.cpp
+++ ch3/3.4.1_Head.cpp
@@ -11,10 +11,17 @@
 #include <vtkCamera.h>
 #include <vtkFixedPointVolumeRayCastMapper.h>
 
-void main()
+#include "vtkAutoInit.h"
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+VTK_MODULE_INIT(vtkInteractionStyle);
+VTK_MODULE_INIT(vtkRenderingFreeType);
+VTK_MODULE_INIT(vtkRenderingVolumeOpenGL2)
+
+int main()
 {
     vtkNew<vtkRenderer> ren;
     vtkNew<vtkRenderWindow> renWin;
+    renWin->AddRenderer(ren);
     vtkNew<vtkRenderWindowInteractor> iren;
     iren->SetRenderWindow(renWin);
 
@@ -31,7 +38,7 @@ void main()
 
     //创建用于vtkVolumeRayCastMapper的光线函数vtkVolumeRayCompositeFunction
     vtkNew<vtkFixedPointVolumeRayCastMapper> volumeMapper;
-    volumeMapper->SetInputData(v16->GetOutput());
+    volumeMapper->SetInputConnection(v16->GetOutputPort());
     
     //创建颜色转化函数vtkColorTransferFunction，用于将像素的强烈程度转化成不同的颜色
     vtkNew<vtkColorTransferFunction> volumeColor;
@@ -48,4 +55,42 @@ void main()
     volumeScalarOpacity->AddPoint(1150,0.85);
 
     //同上，设置不同坡度的透明度
+    vtkNew<vtkPiecewiseFunction> volumeGradientOpacity;
+    volumeGradientOpacity->AddPoint(0  ,0.0);
+    volumeGradientOpacity->AddPoint(90 ,0.5);
+    volumeGradientOpacity->AddPoint(100,1.0);
+
+    //创建vtkVolumeProperty，将颜色及透明度联系起来
+    vtkNew<vtkVolumeProperty> volumeProperty;
+    volumeProperty->SetColor(volumeColor);
+    volumeProperty->SetScalarOpacity(volumeScalarOpacity);
+    volumeProperty->SetGradientOpacity(volumeGradientOpacity);
+    //设置插入数据的插入类型
+    volumeProperty->SetInterpolationTypeToLinear();
+    volumeProperty->ShadeOn();    //对阴暗部分进行计算
+    volumeProperty->SetAmbient(0.4);
+    volumeProperty->SetDiffuse(0.6);
+    volumeProperty->SetSpecular(0.2);
+
+    vtkNew<vtkVolume> volume;
+    volume->SetMapper(volumeMapper);
+    volume->SetProperty(volumeProperty);
+
+    //将体数据集放到体渲染器中
+    ren->AddViewProp(volume);
+
+    //设置相机
+    vtkCamera *camera = ren->GetActiveCamera();
+    double *c = volume->GetCenter();
+    camera->SetFocalPoint(c[0], c[1], c[2]);
+    camera->SetPosition(c[0] + 400, c[1], c[2]);
+    camera->SetViewUp(0,0,-1);
+
+    renWin->SetSize(640, 480);
+
+    //启动渲染交互
+    iren->Initialize();
+    iren->Start();
+
+    return EXIT_SUCCESS;
 }
---
+++ ch3/3.4.2_Reslice.cpp
@@ -0,0 +1,13 @@
+#include <vtkSmartPointer.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkVolume.h>
+#include <vtkVolume16Reader.h>
+#include <vtkVolumeProperty.h>
+#include <vtkColorTransferFunction.h>
+#include <vtkPiecewiseFunction.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkCamera.h>
+#include <vtkFixedPointVolumeRayCastMapper.h>
+
